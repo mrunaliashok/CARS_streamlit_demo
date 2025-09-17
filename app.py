904c310c4f47fd456dd8909b6dc1d8f2c621ea8f
mrunaliashok <mrunali.durugkar@gmail.com>
2025-09-17 13:06:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+# Load dataset
+df = pd.read_csv("../DataSet/CARS.csv")
+
+st.title("Car Horsepower Visualization")
+
+# Show available brands
+brands = df['Make'].unique()
+selected_brand = st.selectbox("Select a Car Brand:", brands)
+
+# Filter data
+s = df[df['Make'] == selected_brand]
+
+# Plot
+fig, ax = plt.subplots(figsize=(10, 5))
+sb.barplot(x=s['Model'], y=s['Horsepower'], ax=ax)
+plt.xticks(rotation=90)
+plt.tight_layout()
+
+# Display chart
+st.pyplot(fig)
